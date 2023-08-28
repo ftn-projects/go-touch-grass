7cc0d367b5776e7d7e663ad854bf237ef77ef2b0
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-08-28 23:06:23 +0200

    add config package

---
+++ config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"os"
+	"runtime"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	path              string
+	SkiplistMaxHeight int
+}
+
+func (c Config) Save() {
+	data, _ := yaml.Marshal(c)
+	os.WriteFile(c.path, data, 0644)
+}
+
+func getConfigPath() string {
+	_, path, _, _ := runtime.Caller(0)
+	return path[:len(path)-len("config.go")] + "config.yaml"
+}
+
+func tryLoad(path string) (*Config, bool) {
+	c := Config{}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, false
+	}
+
+	err = yaml.Unmarshal(data, c)
+	return &c, err != nil
+}
+
+func getDefault() *Config {
+	return &Config{
+		SkiplistMaxHeight: 10,
+	}
+}
+
+func New() *Config {
+	path := getConfigPath()
+	conf, ok := tryLoad(path)
+	if !ok {
+		conf = getDefault()
+	}
+
+	conf.path = path
+	conf.Save()
+	return conf
+}
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	conf "go-touch-grass/config"
 	"go-touch-grass/internal/memtable"
 )
 
 func main() {
-	mt := memtable.Memtable{}
+	mt := memtable.New(conf.Config{})
 	fmt.Println(mt)
+	conf.New()
 }
