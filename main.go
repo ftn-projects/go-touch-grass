a3335481805fed990bfe09c9bc34191181eb4823
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2023-08-29 19:35:13 +0200

    [Add] function creating TOC file

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"go-touch-grass/internal/memtable"
+	"bufio"
+	"go-touch-grass/config"
+	"go-touch-grass/internal/sstable"
+	"os"
 )
 
 func main() {
-	mem := memtable.GetExample()
-	fmt.Println("full: ", mem.IsFull())
-	mem.Print()
+	conf := config.New()
+	sstable.NewSSTable(conf).WriteNewSSTable()
+	table := sstable.GetSSTable(0)
+	scaner := bufio.NewScanner(os.Stdin)
+	for scaner.Scan() {
+		offset := table.Index.Find(scaner.Text())
+		table.Read(offset)
+	}
 }
