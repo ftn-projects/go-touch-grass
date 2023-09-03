2c5790323ce8cc44928d08f0efb46a6d1b9fa36d
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-09-03 21:05:05 +0200

    Add menu test

---
+++ menu/test.go
@@ -0,0 +1,35 @@
+package menu
+
+import (
+	"fmt"
+	"go-touch-grass/internal/app"
+	"os"
+	"strings"
+)
+
+func LoadTestData() {
+	app, err := app.New()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	keys, values := getData()
+	for i := 0; i < len(keys); i++ {
+		app.Put(keys[i], values[i])
+	}
+}
+
+func getData() (keys []string, values [][]byte) {
+	buf, _ := os.ReadFile("menu/test_data.csv")
+	data := string(buf)
+
+	for _, l := range strings.Split(data, "\n") {
+		l := strings.TrimSpace(l)
+		tokens := strings.Split(l, ",")
+
+		keys = append(keys, tokens[0])
+		values = append(values, []byte(tokens[1]))
+	}
+	return
+}
